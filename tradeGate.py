58e44f9de13abb1edc2de283146c600f9939e661
Rustin Soraki <Rustin.soraki@gmail.com>
2022-02-12 21:00:02 +0330

    TradeGate class initialization

--- tradeGate.py
+++ tradeGate.py
@@ -3,6 +3,17 @@ import json
 
 
 class tradeGate():
-    def __init__(self, configFile):
-
-        pass
+    def __init__(self, configAddress):
+        configFile = open(configAddress)
+        config = json.load(configFile)
+        self.apiKey = config['key']
+        self.apiSecret = config['secret']
+        self.apiPassphrase = config['passphrase']
+        
+        self.exchange = ccxt.kucoin({
+            'apiKey': self.apiKey,
+            'secret': self.apiSecret,
+            'password': self.apiPassphrase,
+        })
+        
+        self.markets = self.exchange.loadMarkets()
