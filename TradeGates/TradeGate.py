b3504d326e170fd1cdda18326340246b7e83a939
Rustin Soraki <Rustin.soraki@gmail.com>
2022-02-14 18:38:31 +0330

    Add order base class

--- TradeGates/TradeGate.py
+++ TradeGates/TradeGate.py
@@ -1,5 +1,6 @@
 import ccxt
 import json
+import time
 from Exchanges import BinanceExchange
 
 
@@ -29,3 +30,50 @@ class TradeGate():
         if exchangeName == 'Binance':
             return BinanceExchange.BinanceExchange
 
+class order():
+    def __init__(self, symbol, side, orderType):
+        self.symbol = symbol
+        self.side = side
+        self.orderType = orderType
+
+        self.timeInForce = None
+        self.quantity = None
+        self.quoteOrderQty = None
+        self.price = None
+        self.newClientOrderId = None
+        self.stopPrice = None
+        self.icebergQty = None
+        self.newOrderRespType = None
+        self.recvWindow = None
+
+    def setTimeInForce(self, timeInForce):
+        self.timeInForce = timeInForce
+
+    def setQuantity(self, quantity):
+        self.quantity = quantity
+
+    def setQuoteOrderQty(self, quoteOrderQty):
+        self.quoteOrderQty = quoteOrderQty
+    
+    def setPrice(self, price):
+        self.price = price
+    
+    def setNewClientOrderId(self, newClientOrderId):
+        self.newClientOrderId = newClientOrderId
+
+    def setStopPrice(self, stopPrice):
+        self.stopPrice = stopPrice
+
+    def setIcebergQty(self, icebergQty):
+        self.icebergQty = icebergQty
+    
+    def setNewOrderRespType(self, newOrderRespType):
+        self.newOrderRespType = newOrderRespType
+
+    def setRecvWindow(self, recvWindow):
+        self.recvWindow = recvWindow
+    
+    def setTimeStamp(self):
+        self.timeStamp = time.time()
+
+    
