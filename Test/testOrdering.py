1229109121d938c4efef90cf470ea8a8fd0d765b
Rustin Soraki <Rustin.soraki@gmail.com>
2022-03-02 20:56:59 +0330

    Refactor ordering testing to pytest

--- Test/testOrdering.py
+++ Test/testOrdering.py
@@ -1,95 +1,107 @@
-import unittest
+import pytest
 import json
 from TradeGates.TradeGate import TradeGate
 from TradeGates.Utils.DataHelpers import OrderData
 import logging
 
 
-class BinanceOrderingTest(unittest.TestCase):
-    def setUp(self):
-        with open('./config.json') as f:
-            config = json.load(f)
+def setUp(self):
+    with open('./config.json') as f:
+        config = json.load(f)
 
-        self.tradeGate = TradeGate(config['Binance'], sandbox=True)
-        loglevel = logging.INFO
-        logging.basicConfig(level=loglevel)
-        self.log = logging.getLogger(__name__)
+    self.tradeGate = TradeGate(config['Binance'], sandbox=True)
+    loglevel = logging.INFO
+    logging.basicConfig(level=loglevel)
+    self.log = logging.getLogger(__name__)
 
-    def testNewTestOrder(self):
+@pytest.fixture
+def getGates():
+    gates = []
+    with open('./config.json') as f:
+        config = json.load(f)
+
+    for key in config.keys():
+        gates.append(TradeGate(config[key], sandbox=True))
+
+    return gates
+
+def testNewTestOrder(getGates):
+    for gate in getGates:
         try:
-            res = self.tradeGate.createAndTestOrder('BTCUSDT', 'SELL', 'LIMIT', timeInForce='GTC', quantity=0.002, price=49500)
+            res = gate.createAndTestOrder('BTCUSDT', 'SELL', 'LIMIT', timeInForce='GTC', quantity=0.002, price=49500)
+            assert res is not None, 'Problem in testing making order from {} exchange'.format(gate.exchangeName)
         except Exception as e:
-            self.fail('Problem in order data')
+            assert False, str(e)
 
-    def testNewTestOrderBadOrderType(self):
+def testNewTestOrderBadOrderType(getGates):
+    for gate in getGates:
         try:
-            res = self.tradeGate.createAndTestOrder('BTCUSDT', 'SELL', 'LINIT', timeInForce='GTC', quantity=0.002, price=49500)
+            res = gate.createAndTestOrder('BTCUSDT', 'SELL', 'LINIT', timeInForce='GTC', quantity=0.002, price=49500)
+            assert res is None, 'Bad order type and information provided. Must fail (Exchange: {})'.format(gate.exchangeName) 
         except Exception as e:
-            return
+            assert True, 'Bad order type and information provided. Must fail (Exchange: {})'.format(gate.exchangeName)
+
+def testNewOrder(getGates):
+    for gate in getGates:
+        try:
+            verifiedOrder = gate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
+            result = gate.makeOrder(verifiedOrder)
+            assert result is not None, 'Problem in making new order in {} exchange'.format(gate.exchangeName)
+        except Exception as e:
+            assert False, 'Problem in making new order in {} exchange'.format(gate.exchangeName)
+
+def testGetOrders(getGates):
+    for gate in getGates:
+        orders = gate.getSymbolOrders('BTCUSDT')
+        assert orders is not None, 'Problem in getting list of all orders from {} exchange.'.format(gate.exchangeName)
+
+def testGetOpenOrders(getGates):
+    for gate in getGates:
+        openOrders = gate.getOpenOrders()
+        assert openOrders is not None, 'Problem in getting list of open orders without symbol from {} exchange.'.format(gate.exchangeName)
+
+        symbolOpenOrders = gate.getOpenOrders('BTCUSDT')
+        assert symbolOpenOrders is not None, 'Problem in getting list of open orders with symbol from {} exchange.'.format(gate.exchangeName)
+
+def testGetOrder(getGates):
+    for gate in getGates:
+        try:
+            verifiedOrder = gate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
+            result = gate.makeOrder(verifiedOrder)
+        except Exception as e:
+            assert False, 'Problem in making order from {} exchange: {}'.format(gate.exchangeName, str(e))
         
-        self.fail('Problem in validating order data')
+        order = gate.getOrder('BTCUSDT', orderId=result['orderId'])
+        assert order['clientOrderId'] == result['clientOrderId'], 'Fetch client orderID is not equal to the actual client orderID from {} exchange.'.format(gate.exchangeName)
 
-    def testNewOrder(self):
+        order = gate.getOrder('BTCUSDT', localOrderId=result['clientOrderId'])
+        assert order['orderId'] == result['orderId'], 'Fetch orderID is not equal to the actual orderID from {} exchange.'.format(gate.exchangeName)
+
+        gate.cancelSymbolOpenOrder('BTCUSDT', orderId=result['orderId'])
+
+def testCancelingAllOpenOrders(getGates):
+    for gate in getGates:
+        result = gate.cancelAllSymbolOpenOrders('BTCUSDT')
+
+        openOrders = gate.getOpenOrders('BTCUSDT')
+        assert len(openOrders) == 0, 'Problem in canceling all Open Orders in {} exchange.'.format(gate.exchangeName)
+
+def testCancelingOrder(getGates):
+    for gate in getGates:
         try:
-            verifiedOrder = self.tradeGate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
+            verifiedOrder = gate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
+            result = gate.makeOrder(verifiedOrder)
         except Exception as e:
-            self.fail('Problem in order data: {}'.format(str(e)))
+            assert False, 'Problem in making order in {} exchange: {}'.format(gate.exchangeName, str(e))
 
-        try:
-            result = self.tradeGate.makeOrder(verifiedOrder)
-            # self.log.info('\nOrder status: {}'.format(result))
-        except Exception as e:
-            self.fail('Problem in making order: {}'.format(str(e)))
-
-    def testGetOrders(self):
-        # self.log.info('\nOrders: {}'.format(self.tradeGate.getSymbolOrders('BTCUSDT')))
-        self.assertIsNotNone(self.tradeGate.getSymbolOrders('BTCUSDT'), 'Problem in getting list of all orders')
-
-    def testGetOpenOrders(self):
-        # self.log.info('\nOrders: {}'.format(self.tradeGate.getOpenOrders('BTCUSDT')))
-        self.assertIsNotNone(self.tradeGate.getOpenOrders(), 'Problem in getting list of open orders without symbol.')
-        self.assertIsNotNone(self.tradeGate.getOpenOrders('BTCUSDT'), 'Problem in getting list of open orders with symbol.')
-
-    def testGetOrder(self):
-        try:
-            verifiedOrder = self.tradeGate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
-            result = self.tradeGate.makeOrder(verifiedOrder)
-        except Exception as e:
-            self.fail('Problem in making order: {}'.format(str(e)))
-        
-        order = self.tradeGate.getOrder('BTCUSDT', orderId=result['orderId'])
-        self.assertEqual(order['clientOrderId'], result['clientOrderId'])
-
-        order = self.tradeGate.getOrder('BTCUSDT', localOrderId=result['clientOrderId'])
-        self.assertEqual(order['orderId'], result['orderId'])
-
-        self.tradeGate.cancelSymbolOpenOrder('BTCUSDT', orderId=result['orderId'])
-
-    def testCancelingAllOpenOrders(self):
-        self.testNewOrder()
-        result = self.tradeGate.cancelAllSymbolOpenOrders('BTCUSDT')
-        # self.log.info('\n{}'.format(result))
-
-        openOrders = self.tradeGate.getOpenOrders('BTCUSDT')
-        self.assertEqual(len(openOrders), 0, 'Problem in canceling all Open Orders')
-
-    def testCancelingOrder(self):
-        try:
-            verifiedOrder = self.tradeGate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
-            result = self.tradeGate.makeOrder(verifiedOrder)
-        except Exception as e:
-            self.fail('Problem in making order: {}'.format(str(e)))
-        result = self.tradeGate.cancelSymbolOpenOrder(symbol='BTCUSDT', orderId=result['orderId'])
-        self.assertEqual(result['status'], 'CANCELED', 'Problem in canceling specified Open Orders')
+        result = gate.cancelSymbolOpenOrder(symbol='BTCUSDT', orderId=result['orderId'])
+        assert result['status'] == 'CANCELED', 'Problem in canceling specified Open Orders in {} exchange.'.format(gate.exchangeName)
 
         try:
-            verifiedOrder = self.tradeGate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
-            result = self.tradeGate.makeOrder(verifiedOrder)
+            verifiedOrder = gate.createAndTestOrder('BTCUSDT', 'BUY', 'LIMIT', quantity=0.002, price=35000, timeInForce='GTC')
+            result = gate.makeOrder(verifiedOrder)
         except Exception as e:
-            self.fail('Problem in making order: {}'.format(str(e)))
-        result = self.tradeGate.cancelSymbolOpenOrder(symbol='BTCUSDT', localOrderId=result['clientOrderId'])
-        self.assertEqual(result['status'], 'CANCELED', 'Problem in canceling specified Open Orders')
+            assert False, 'Problem in making order in {} exchange: {}'.format(gate.exchangeName, str(e))
 
-
-if __name__ == '__main__':
-    unittest.main()
+        result = gate.cancelSymbolOpenOrder(symbol='BTCUSDT', localOrderId=result['clientOrderId'])
+        assert result['status'] == 'CANCELED', 'Problem in canceling specified Open Orders in {} exchange.'.format(gate.exchangeName)
